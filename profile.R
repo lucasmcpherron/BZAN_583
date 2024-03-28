086f181fbb5f42c3f1f37dcacdb682a44603fd68
Lucas McPherron <lucas.mcpherron@icloud.com>
2024-03-28 16:11:35 -0400

    HW2_pt2

--- profile.R
+++ profile.R
@@ -1,63 +1,52 @@
 Rprof()
-KUB_22_5min <- KUB_22_5min %>% 
-  mutate(WATER_FLOW = ifelse(WATER_FLOW == 0, NA, WATER_FLOW))
-KUB_23_5min <- KUB_23_5min %>% 
-  mutate(WATER_FLOW = ifelse(WATER_FLOW == 0, NA, WATER_FLOW))
 
-KUB_22_5min[which.min(KUB_22_5min$WATER_FLOW),]
-KUB_23_5min[which.min(KUB_23_5min$WATER_FLOW),]
+par_course <- numeric(10000)
+total_strokes_round <- numeric(10000)
+total_score_round <- numeric(10000)
 
-sum(KUB_22_5min$WATER_FLOW==0)
-sum(KUB_23_5min$WATER_FLOW==0)
-
-for (i in seq(1, nrow(KUB_22_5min)-2, by=3)) {
-  avg_value <- mean(KUB_22_5min$WATER_FLOW[i:(i+2)])
-  KUB_22_15min$WATER_FLOW[(i+2)/3] <- avg_value
+# Simulate the golf round 10,000 times
+for (sim in 1:10000) {
+  total_strokes <- 0
+  total_score <- 0
+  
+  # Initialize a dataframe to store par and strokes for each hole
+  scorecard <- data.frame(Hole = 1:18, Par = integer(18), Strokes = 
+integer(18))
+  
+  # Simulate each hole
+  for (hole in 1:18) {
+    # Choose par for the hole
+    par <- sample(c(3, 4, 5), 1, prob = c(0.4, 0.4, 0.2))
+    
+    # Simulate strokes for the hole
+    strokes_to_par <- sample(c(-2, -1, 0, 1, 2, 3), 1, prob = c(0.03, 
+0.17, 0.55, 0.18, 0.05, 0.02)) + par - 4
+    
+    # Store par and strokes for the hole in the scorecard dataframe
+    scorecard$Par[hole] <- par
+    scorecard$Strokes[hole] <- strokes_to_par
+    
+    # Update total strokes and total score relative to par
+    total_strokes <- total_strokes + strokes_to_par + par
+    total_score <- total_score + (strokes_to_par + par)
+  }
+  
+  # Store results for the current iteration
+  par_course[sim] <- sum(scorecard$Par)
+  total_strokes_round[sim] <- total_strokes
+  total_score_round[sim] <- total_strokes - sum(scorecard$Par)
 }
 
-for (i in seq(1, nrow(KUB_23_5min)-2, by=3)) {
-  avg_value <- mean(KUB_23_5min$WATER_FLOW[i:(i+2)])
-  KUB_23_15min$WATER_FLOW[(i+2)/3] <- avg_value
-}
+# Calculate averages
+avg_par_course <- mean(par_course)
+avg_total_strokes <- mean(total_strokes_round)
+avg_total_score <- mean(total_score_round)
 
-for (i in seq(1, nrow(KUB_22_hourly)-1)) {
-  avg_value <- mean(KUB_22_hourly$Raw_Temp[i:(i+1)])
-  # Update the new 15-minute interval column with the averaged values
-  KUB_22_15min$Raw_Temp[i*4 - 3] <- avg_value
-  KUB_22_15min$Raw_Temp[i*4 - 2] <- avg_value
-  KUB_22_15min$Raw_Temp[i*4 - 1] <- avg_value
-  KUB_22_15min$Raw_Temp[i*4] <- avg_value
-}
-
-for (i in seq(1, nrow(KUB_23_hourly)-1)) {
-  avg_value <- mean(KUB_23_hourly$Raw_Temp[i:(i+1)])
-  # Update the new 15-minute interval column with the averaged values
-  KUB_23_15min$Raw_Temp[i*4 - 3] <- avg_value
-  KUB_23_15min$Raw_Temp[i*4 - 2] <- avg_value
-  KUB_23_15min$Raw_Temp[i*4 - 1] <- avg_value
-  KUB_23_15min$Raw_Temp[i*4] <- avg_value
-}
-
-KUB_22_15min <- KUB_22_15min %>%
-  mutate(CDG1_FEED_RATE_LBS_HR = ifelse(ChlorineDioxide1_RATE_LBS_HR == 1, 0, ChlorineDioxide1_RATE_LBS_HR),
-         CDG2_FEED_RATE_LBS_HR = ifelse(ChlorineDioxide2_RATE_LBS_HR == 1, 0, ChlorineDioxide2_RATE_LBS_HR),
-         CDG_FEED_RATE_LBS_HR_TOTAL = CDG1_FEED_RATE_LBS_HR + CDG2_FEED_RATE_LBS_HR) 
-
-# Polymer feed and avg dose are zero until 2022-11-01 in KUB_22_daily
-# head(KUB_22_daily)
-KUB_22_daily <- KUB_22_daily %>%
-  mutate(Polymer_Feed_Gals = ifelse(Polymer_Feed_Gals == 0, NA, Polymer_Feed_Gals),
-         Polymer_Feed_Avg_Dose = ifelse(Polymer_Feed_Avg_Dose == 0, NA, Polymer_Feed_Avg_Dose))
-
-KUB_23_15min <- KUB_23_15min %>%
-  mutate(ChlorineDioxide1_RATE_LBS_HR = ifelse(ChlorineDioxide1_RATE_LBS_HR == 1, 0, ChlorineDioxide1_RATE_LBS_HR),
-         ChlorineDioxide2_RATE_LBS_HR = ifelse(ChlorineDioxide2_RATE_LBS_HR == 1, 0, ChlorineDioxide2_RATE_LBS_HR),
-         ChlorineDioxide_RATE_LBS_HR_TOTAL = ChlorineDioxide1_RATE_LBS_HR + ChlorineDioxide2_RATE_LBS_HR)
-
-KUB_22_15min <- KUB_22_15min %>%
-  mutate(ChlorineDioxide1_RATE_LBS_HR = ifelse(ChlorineDioxide1_RATE_LBS_HR == 1, 0, ChlorineDioxide1_RATE_LBS_HR),
-         ChlorineDioxide2_RATE_LBS_HR = ifelse(ChlorineDioxide2_RATE_LBS_HR == 1, 0, ChlorineDioxide2_RATE_LBS_HR),
-         ChlorineDioxide_RATE_LBS_HR_TOTAL = ChlorineDioxide1_RATE_LBS_HR + ChlorineDioxide2_RATE_LBS_HR)
+# Print the results
+cat("Average par for the course:", avg_par_course, "\n")
+cat("Average total strokes for the round:", avg_total_strokes, "\n")
+cat("Average total score relative to par for the round:", avg_total_score, 
+"\n")
 
 Rprof(NULL)
 summaryRprof()
